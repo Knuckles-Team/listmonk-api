67c80ad5994a753a22176e60653f194e409b42e8
knucklessg1 <knucklessg1@gmail.com>
2026-05-22 08:19:39 -0500

    Bump version: 0.5.0 → 0.6.0

--- listmonk_api/mcp_server.py
+++ listmonk_api/mcp_server.py
@@ -33,7 +33,7 @@ from dotenv import find_dotenv, load_dotenv
 
 from listmonk_api.auth import get_client
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 logger = get_logger(name="ListmonkMCP")
 logger.setLevel(logging.DEBUG)
 
